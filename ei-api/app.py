f29b4e75d3870ca921986227219c9e0eb0c77fef
Aaron Carson <aaron@aaroncarson.co.uk>
2023-02-04 16:02:43 +0000

    Update protobuf (closes #27)

--- ei-api/app.py
+++ ei-api/app.py
@@ -2,32 +2,35 @@ import sentry_sdk
 from sentry_sdk.integrations.falcon import FalconIntegration
 
 sentry_sdk.init(
-  dsn="https://d42921cbb87b4c26b1a91dc566d811f2@o915576.ingest.sentry.io/6596181",
-  integrations=[
-     FalconIntegration(),
-  ],
-
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production,
-  traces_sample_rate=1.0,
+    dsn="https://d42921cbb87b4c26b1a91dc566d811f2@o915576.ingest.sentry.io/6596181",
+    integrations=[
+        FalconIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=0.1,
 )
 
 import falcon
 
 import sys
-sys.path.append('ei-api/')
+
+sys.path.append("ei-api/")
 
 app = application = falcon.App()
 
 from .stats import StatsResource
+
 stats = StatsResource()
-app.add_route('/stats', stats)
+app.add_route("/stats", stats)
 
 from .contracts import ContractResource
+
 contracts = ContractResource()
-app.add_route('/contracts', contracts)
+app.add_route("/contracts", contracts)
 
 from .full_output import FullBackupResource
+
 full_backup = FullBackupResource()
-app.add_route('/full_backup', full_backup)
+app.add_route("/full_backup", full_backup)
